bb7f02a367e638e8ef1c517e81a648ed83b0f943
Suketh-Ketham <suketh558@gmail.com>
2026-03-18 19:27:06 +0530

    Initial commit

---
+++ Week 08/User-Management-App/Frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import RootLayout from "./components/RootLayout"
+import Home from "./components/Home"
+import User from "./components/User"
+import UsersList from "./components/UsersList"
+import AddUser from "./components/AddUser";
+
+function App() {
+  const routerObj = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "user",
+        element: <User />
+      },
+      {
+        path: "users",
+        element: <UsersList />
+      },
+      {
+        path: "add-user",
+        element: <AddUser />
+      }
+    ]
+  }
+])
+
+  return <RouterProvider router={routerObj} />
+}
+
+export default App
